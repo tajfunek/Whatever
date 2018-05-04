fcc3791c86133f1cba762664a729b79cb6b6ca4a
tajfunek <rafallis95@gmail.com>
2018-05-04 17:43:37 +0000

    beter png.c but still not working

--- png.c
+++ png.c
@@ -1,28 +1,46 @@
 #include "png2.h"
 
-PyObject* read_png(PyObject *self, PyObject *args) {
+PyObject* read_PNG(PyObject *self, PyObject *args) {
   //clock_t start = clock()
 
   // Parsing arguments
   const char* filename;
-  if(!PyArgs_ParseTuple(args, "s", filename)) {
+  if(!PyArg_ParseTuple(args, "s", &filename)) {
     printf("Error while parsing args");
     return NULL;
   }
 
+  printf("Filename: %s \n", filename);
+
   // Reading file
   FILE* fp = fopen(filename, "rb");
   if (!fp) {
-    return 9;
+    printf("Error while reading file");
+    return NULL;
   }
+
+  printf("File read\n\n");
+  int i = 0;
+
+  fread(header, 1, 8, fp);
+  if (png_sig_cmp(header, 0, 8))
+    abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+
+  printf("filetype correct");
+
   // Initializing basic varibles
   png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
   png_infop info = png_create_info_struct(png);
   setjmp(png_jmpbuf(png));
 
+  printf("Image initialized:  %i\n\n", i++);
+
   // Connetcing file to PNG_Structp
   png_init_io(png, fp);
+  png_set_sig_bytes(png_ptr, 8);
   png_read_info(png, info);
+
+  printf("Image read\n\n");
 
   // Closing file
   fclose(fp);
@@ -33,7 +51,7 @@ PyObject* read_png(PyObject *self, PyObject *args) {
   png_byte color_type = png_get_color_type(png, info);
   png_byte bit_depth = png_get_bit_depth(png, info);
 
-  // Chceking image
+  // Checking image
   if(bit_depth == 16)
     png_set_strip_16(png);
 
@@ -59,14 +77,21 @@ PyObject* read_png(PyObject *self, PyObject *args) {
 
   png_read_update_info(png, info);
 
+  printf("Sth has been don\n\n\");
+
   // Reading image
   png_bytep *row_pointers;
   row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
-  for(int y = 0; y < height; y++) {
+  int y;
+  printf("Row pointer init\n\n");
+  for(y = 0; y < height; y++) {
     row_pointers[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
   }
+  printf("png_row_bytes\n\n");
 
   png_read_image(png, row_pointers);
+
+  printf("Row pointer done\n\n");
 
   int length = width * height * 4;
 
@@ -75,20 +100,48 @@ PyObject* read_png(PyObject *self, PyObject *args) {
   unsigned char* pixels = malloc(length * sizeof(unsigned char));
   for(i = 0; i < height; i++) {
     for(j = 0; j < width; j++) {
-      pixels[i*height + j] = row_pointers[i][j]
+      pixels[i*height + j] = row_pointers[i][j];
     }
   }
 
   // Creating Python List and filling it with metadata
   PyObject* list = PyList_New((Py_ssize_t)length);
   for(i = 0; i < length; i++) {
-    if(PyList_SetItem(list, (Py_ssize_t)i, pixels[i]) == -1) {
+    PyObject* pixel;
+    if((pixel = PyLong_FromSize_t((size_t)pixels[i])) == NULL) {
+      printf("Error while PyLonging");
+      return NULL;
+    }
+    if(PyList_SetItem(list, (Py_ssize_t)i, pixel) == -1) {
       printf("Error while filling up list");
       return NULL;
     }
-
-    return list;
   }
 
-  return;
+  return list;
 }
+
+
+// Mapping between python and c function names.
+static PyMethodDef readModule_methods[] = {
+    {"read", read_PNG, METH_VARARGS, "Reads PNG image ASAP"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef pngmodule = {
+    PyModuleDef_HEAD_INIT,
+    "PNG_read",
+    NULL,
+    -1,
+    readModule_methods
+};
+
+// Module initialisation routine.
+PyMODINIT_FUNC
+PyInit_PNG_read(void) {
+    // Init module.
+    return PyModule_Create(&pngmodule);
+
+}
+
+
