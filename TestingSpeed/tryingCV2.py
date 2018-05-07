c126dee27f35f3ccf4ecfdea4b0b7d45ff213145
Rafał "tajfunek" Lis <rafallis95@gmail.com>
2018-05-07 18:17:47 +0200

    Added time to trying modules

--- TestingSpeed/tryingCV2.py
+++ TestingSpeed/tryingCV2.py
@@ -1,6 +1,8 @@
 import cv2
+import time
+_time = time.time()
 for i in range(10000):
     img = cv2.imread('try.png')
-
+print("Time: ", time.time() - _time, "\n")
 print("open CV")
 #print(img)
--- tryingTajfunek.py
+++ tryingTajfunek.py
@@ -1,6 +1,8 @@
 import PNG_read
+import time
+_time = time.time()
 for i in range(1000):
     pixels = PNG_read.read("try.png")
-
+print("Time: ", time.time() - _time, "\n")
 print("Tajfunek's PNG_read")
 #print(pixels)
