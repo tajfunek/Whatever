51d9d2a1952e70e75ba9c2fe95cbf711bde96e65
Rafał "tajfunek" Lis <rafallis95@gmail.com>
2018-05-08 14:54:42 +0200

    Error corrections

--- hardware.c
+++ hardware.c
@@ -93,11 +93,12 @@ int main(void) {
 
   // Binding with file
   printf("Preparation for binding...\n");
-  struct sockaddr_un* addr;
-  *(addr).sun_family = AF_UNIX;
-  strncpy(*(addr).sun_path, "./temp", sizeof(*(addr).sun_path)-1);
+  struct sockaddr_un addr= {
+    .sun_family = AF_UNIX,
+    .sun_path = "./temp"
+  //strncpy(*(addr).sun_path, "./temp", sizeof(*(addr).sun_path)-1);
   printf("Bind\n");
-  if(bind(socket_d, (struct sockaddr*)addr, sizeof(struct sockaddr_un)) != 0) {
+  if(bind(socket_d, (struct sockaddr*)&addr, sizeof(struct sockaddr_un)) != 0) {
     printf("Unable to bind file to socket");
     abort();
   }
@@ -117,7 +118,7 @@ int main(void) {
     struct cam_args_t* args;
     sprintf(args->cam, "/dev/video%i", i);
     args->socket = socket_d;
-    args->addr = (struct sockaddr*)addr;
+    args->addr = (struct sockaddr*)&addr;
 
     // Creating thread and checking for error
     if(pthread_create(&cams[i], NULL, &cam, args) != SUCCESS) {
@@ -130,7 +131,7 @@ int main(void) {
   printf("Creating thread for motor\n");
   struct motor_args_t* args;
   args->socket = socket_d;
-  args->addr = (struct sockaddr*)addr;
+  args->addr = (struct sockaddr*)&addr;
 
   // Create thread for servo controlling
   pthread_t* motor_thread;
@@ -147,7 +148,7 @@ int main(void) {
   char buf[20];
   size_t socklen = sizeof(struct sockaddr_un);
   for(int i = 0; i < THREADS_NO; /* INSIDE LOOP */) {
-    if((ids[i]->socket = accept(socket_d, (struct sockaddr*)addr, (socklen_t*)&socklen)) != -1) {
+    if((ids[i]->socket = accept(socket_d, (struct sockaddr*)&addr, (socklen_t*)&socklen)) != -1) {
       recv(ids[i]->socket,buf, sizeof(buf), 0);
       strncpy(ids[i]->id, buf, 15);
       printf("Connected to %s\n", ids[i]->id);
