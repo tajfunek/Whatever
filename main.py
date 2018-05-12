6bc6f0c8b344656fc56e2e27650164fc11aece6d
Roundhousekicker <mj.jedrych@gmail.com>
2018-05-12 15:11:22 +0200

    fixing main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ driver.setup()
 GPIO.setup((21, 20), GPIO.OUT)
 print("Starting...\n")
 stepdeg = 16 # resolution
-motor_time = 10 # in ms
+motor_time = 10 # in ms, '10' Can be changed to lower if works
 turns = int(64*64/stepdeg)
 
 for i in range(turns):
@@ -44,60 +44,60 @@ for i in range(turns):
         for line in error1.splitlines():
             if line.startswith('Error') or line.startswith('stat'):
                 error = 1
-                break
+                #break
 
-        # Next camera
-        #print("CAM2")
-        error = 1
-        while error:
-            error = 0
+    # Next camera
+    #print("CAM2")
+    error = 1
+    while error:
+        error = 0
             #print("Iteration")
-            cam1 = s.Popen(shlex.split("sudo fswebcam --resolution 1280x1024 --device /dev/video1 \
-             --no-banner --png --no-title --no-subtitle --no-timestamp --no-info \
-             images_new/2_{}.png".format(i)), stdout = s.PIPE, stderr = s.PIPE)
-            try:
-                #print("Communication")
-                _, error1 = cam1.communicate(timeout = 10)
-            except:
-                cam1.kill()
-                print("ERROR")
-                sys.exit()
-            finally:
-                pass
-                #print("Done")
-            error1 = error1.decode('utf-8')
-            for line in error1.splitlines():
-                if line.startswith('Error') or line.startswith('stat'):
-                    error = 1
-                    break
+        cam1 = s.Popen(shlex.split("sudo fswebcam --resolution 1280x1024 --device /dev/video1 \
+         --no-banner --png --no-title --no-subtitle --no-timestamp --no-info \
+         images_new/2_{}.png".format(i)), stdout = s.PIPE, stderr = s.PIPE)
+        try:
+            #print("Communication")
+            _, error1 = cam1.communicate(timeout = 10)
+        except:
+            cam1.kill()
+            print("ERROR")
+            sys.exit()
+        finally:
+            pass
+            #print("Done")
+        error1 = error1.decode('utf-8')
+        for line in error1.splitlines():
+            if line.startswith('Error') or line.startswith('stat'):
+                error = 1
+                #break
 
-        #print("CAM3")
-        GPIO.output(21, 0)
-        GPIO.output(20, 1)
-        error = 1
-        while error:
-            error = 0
-            #print("Iteration")
-            cam1 = s.Popen(shlex.split("sudo fswebcam --resolution 1280x1024 --device /dev/video2 \
-             --no-banner --png --no-title --no-subtitle --no-timestamp --no-info \
-             images_new/3_{}.png".format(i)), stdout = s.PIPE, stderr = s.PIPE)
-            try:
-                #print("Communication")
-                _, error1 = cam1.communicate(timeout = 10)
-            except:
-                cam1.kill()
-                print("ERROR")
-                sys.exit()
-            finally:
-                pass
-                #print("Done")
-            error1 = error1.decode('utf-8')
-            for line in error1.splitlines():
-                if line.startswith('Error') or line.startswith('stat'):
-                    error = 1
-                    break
+    #print("CAM3")
+    GPIO.output(21, 0)
+    GPIO.output(20, 1)
+    error = 1
+    while error:
+        error = 0
+        #print("Iteration")
+        cam1 = s.Popen(shlex.split("sudo fswebcam --resolution 1280x1024 --device /dev/video2 \
+         --no-banner --png --no-title --no-subtitle --no-timestamp --no-info \
+         images_new/3_{}.png".format(i)), stdout = s.PIPE, stderr = s.PIPE)
+        try:
+            #print("Communication")
+            _, error1 = cam1.communicate(timeout = 10)
+        except:
+            cam1.kill()
+            print("ERROR")
+            sys.exit()
+        finally:
+            pass
+            #print("Done")
+        error1 = error1.decode('utf-8')
+        for line in error1.splitlines():
+            if line.startswith('Error') or line.startswith('stat'):
+                error = 1
+                #break
 
-        driver.forward(motor_time/1000, stepdeg) # '10' Can be changed to lower if works
+    driver.forward(motor_time/1000, stepdeg) # '10' Can be changed to lower if works
 
 
 print("TIME: ", t.time() - _t)
