cbd7c25fe8de82c659f488fc4f726693b1b75d39
tajfunek <rafallis95@gmail.com>
2018-05-20 19:48:42 +0000

    sth

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ _t=t.time()
 # GPIO.setwarnings(False)
 os.nice(-5)
 driver.setup()
-GPIO.setup((21, 20), GPIO.OUT)
+GPIO.setup((21, 20, 26), GPIO.OUT)
 print("Starting...\n")
 stepdeg = 4 # resolution - sth is wrong 64*8 is 360deg. not 64*64
 motor_time = 10 # in ms, '10' Can be changed to lower if works
