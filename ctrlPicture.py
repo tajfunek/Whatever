4d0a0ce4301daf4b2f4bed5999268566e865cbf7
Roundhousekicker <mj.jedrych@gmail.com>
2018-05-23 17:09:53 +0200

    poprawka do fswebcam

---
+++ ctrlPicture.py
@@ -0,0 +1,8 @@
+import os
+import subprocess as s
+import shlex
+import sys
+
+cam1 = s.Popen(shlex.split("sudo fswebcam --resolution 1280x1024 --device /dev/video0 \
+--no-banner --png --no-title --no-subtitle --no-timestamp --no-info --rotate 90\
+ ctrlPicture.png", stdout = s.PIPE, stderr = s.PIPE)
